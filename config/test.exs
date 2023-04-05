58ab0d476dd9b7812dab23f8d2ebe3feb6a3eb72
twozeronine <amdusias209@gmail.com>
2023-04-05 22:29:46 +0900

    add postgresql & refactoring

--- config/test.exs
+++ config/test.exs
@@ -1,30 +1,21 @@
 import Config
 
-# Configure your database
-#
-# The MIX_TEST_PARTITION environment variable can be used
-# to provide built-in test partitioning in CI environment.
-# Run `mix help test` for more information.
 config :my_plate_slate, MyPlateSlate.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
   database: "my_plate_slate_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: 10
+  pool_size: 10,
+  port: 25432
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
 config :my_plate_slate, MyPlateSlateWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4002],
   secret_key_base: "E2BZ3pfATk8ccrMrzTyMbZQfuT2AkX+buDJBLDTKj8j0e1+aakzWKm8g3ubnVXFT",
   server: false
 
-# In test we don't send emails.
 config :my_plate_slate, MyPlateSlate.Mailer, adapter: Swoosh.Adapters.Test
 
-# Print only warnings and errors during test
 config :logger, level: :warn
 
-# Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
